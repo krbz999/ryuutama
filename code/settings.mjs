8326a00d2941f2ad0a60b846d3bc0dcf661d655c
Zhell <kennethzhell@gmail.com>
2025-10-05 21:28:35 +0200

    [#6] Add setting and UI button to change current terrain / weather.

--- code/settings.mjs
+++ code/settings.mjs
@@ -1,3 +1,5 @@
+const { ForeignDocumentField, SchemaField, StringField } = foundry.data.fields;
+
 export default function registerSettings() {
   // Storing the primary party's id.
   game.settings.register(ryuutama.id, "primaryParty", {
@@ -9,17 +11,103 @@ export default function registerSettings() {
     config: false,
     onChange: () => ui.actors.render(),
   });
+
+  // Current habitat.
+  game.settings.register(ryuutama.id, "currentHabitat", {
+    name: "Current Habitat",
+    scope: "world",
+    requiresReload: false,
+    type: new SchemaField({
+      terrain: new StringField({ required: true, blank: true, choices: () => ryuutama.config.terrainTypes }),
+      weather: new StringField({ required: true, blank: true, choices: () => ryuutama.config.weatherTypes }),
+    }),
+    default: { terrain: "", weather: "" },
+    config: false,
+    onChange: value => onChangeHabitat(value),
+  });
 }
+
+/* -------------------------------------------------- */
 
 class PrimaryPartyModel extends foundry.abstract.DataModel {
   /** @inheritdoc */
   static defineSchema() {
     return {
-      actor: new foundry.data.fields.ForeignDocumentField(foundry.documents.BaseActor, {
+      actor: new ForeignDocumentField(foundry.documents.BaseActor, {
         blank: true,
         validate: id => !game.actors || (game.actors.get(id)?.type === "party"),
         validationError: "This is not a valid id for a Party-type actor.",
       }),
     };
   }
+}
+
+/* -------------------------------------------------- */
+
+/**
+ * Create a button above the Players to configure the current habitat.
+ */
+Hooks.once("renderPlayers", (players) => {
+  if (!game.user.isGM) return;
+
+  /** @type {HTMLButtonElement} */
+  const element = players.element.ownerDocument.createElement("BUTTON");
+  element.type = "button";
+  element.classList.add("faded-ui");
+  element.id = "ryuutama-current-habitat";
+
+  const value = game.settings.get(ryuutama.id, "currentHabitat");
+  onChangeHabitat(value, element);
+
+  players.element.insertAdjacentElement("beforebegin", element);
+
+  element.addEventListener("click", configureHabitat);
+});
+
+/* -------------------------------------------------- */
+
+/**
+ * Prompt a dialog to configure the habitat setting.
+ * @returns {Promise<void>}
+ */
+async function configureHabitat() {
+  const fields = [];
+  const current = game.settings.get(ryuutama.id, "currentHabitat");
+  for (const field of game.settings.settings.get("ryuutama.currentHabitat").type) {
+    fields.push(field.toFormGroup({}, { value: current[field.name] }));
+  }
+
+  let habitat = await foundry.applications.api.Dialog.input({
+    content: fields.map(field => field.outerHTML).join(""),
+  });
+  if (!habitat) return;
+
+  habitat = foundry.utils.expandObject(habitat)[ryuutama.id].currentHabitat;
+
+  game.settings.set(ryuutama.id, "currentHabitat", habitat);
+}
+
+/* -------------------------------------------------- */
+
+/**
+ * Respond to the habitat setting being changed, or the creation of the button.
+ * @param {{ terrain: string, weather: string}} value   Value to derive a label from.
+ * @param {HTMLButtonElement} [button]                  The button, if being created.
+ */
+function onChangeHabitat(value, button = null) {
+  button ??= document.getElementById("ryuutama-current-habitat");
+
+  let { terrain, weather } = value;
+
+  let text = `<span>${game.i18n.localize("RYUUTAMA.HABITAT.currentHabitat")}</span>`;
+  let current = [];
+
+  terrain = ryuutama.config.terrainTypes[terrain]?.label;
+  if (terrain) current.push(terrain);
+
+  weather = ryuutama.config.weatherTypes[weather]?.label;
+  if (weather) current.push(weather);
+
+  if (current.length) text += `<span>${current.join(" + ")}</span>`;
+  button.innerHTML = text;
 }
