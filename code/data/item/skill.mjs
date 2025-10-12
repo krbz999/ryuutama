ae4371c3d5305e11d13207ba80ca595df2eae7c4
Zhell <kennethzhell@gmail.com>
2025-10-12 14:50:17 +0200

    Reduce `SkillData` model.

--- code/data/item/skill.mjs
+++ code/data/item/skill.mjs
@@ -1,16 +1,11 @@
-const { HTMLField, SchemaField, SetField, StringField } = foundry.data.fields;
+const { HTMLField, SchemaField } = foundry.data.fields;
 
 export default class SkillData extends foundry.abstract.TypeDataModel {
   /** @override */
   static defineSchema() {
     return {
-      abilities: new SetField(new StringField({ choices: () => ryuutama.config.abilityScores })),
-      activation: new StringField({ required: true }),
       description: new SchemaField({
         value: new HTMLField(),
-      }),
-      target: new SchemaField({
-        custom: new StringField({ required: true }),
       }),
     };
   }
