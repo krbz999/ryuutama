63c3de9b2b04e9de6adac8c50f46e48730d2ab12
Zhell <kennethzhell@gmail.com>
2025-11-06 01:16:27 +0100

    Adjust SVG. Use SVG instead of IMG.

--- code/applications/ui/game-pause.mjs
+++ code/applications/ui/game-pause.mjs
@@ -8,4 +8,16 @@ export default class RyuutamaGamePause extends foundry.applications.ui.GamePause
     });
     return context;
   }
+
+  /* -------------------------------------------------- */
+
+  /** @override */
+  async _renderHTML(context, options) {
+    const img = document.createElement("ryuutama-icon");
+    img.src = context.icon;
+    if (context.spin) img.classList.add("fa-spin");
+    const caption = document.createElement("figcaption");
+    caption.innerText = context.text;
+    return [img, caption];
+  }
 }
