73395c64b0be75a5f2a860820866c58691ae30e4
Zhell <kennethzhell@gmail.com>
2025-11-16 13:42:42 +0100

    [#52] Sort exports. Move second class advancement to correct level.

--- code/utils/_module.mjs
+++ code/utils/_module.mjs
@@ -1,9 +1,9 @@
 export { default as applyStatus } from "./apply-status.mjs";
+export { default as createDefaultIdentifier } from "./create-default-identifier.mjs";
+export { default as isValidIdentifier } from "./is-valid-identifier.mjs";
 export { default as parseInputDelta } from "./parse-input-delta.mjs";
 export { default as prelocalize } from "./prelocalize.mjs";
 export { default as staticId } from "./static-id.mjs";
-export { default as createDefaultIdentifier } from "./create-default-identifier.mjs";
-export { default as isValidIdentifier } from "./is-valid-identifier.mjs";
 
 export { default as PseudoDocumentCollection } from "./pseudo-document-collection.mjs";
 export * as advancement from "./advancement/_module.mjs";
