c0ec2064f30f57520df55187eec556577c7e9e2e
Zhell <kennethzhell@gmail.com>
2025-11-20 21:28:01 +0100

    Add default icon for half the item types.

--- code/documents/item.mjs
+++ code/documents/item.mjs
@@ -7,4 +7,42 @@ export default class RyuutamaItem extends foundry.documents.Item {
     if (this.system.identifier) return this.system.identifier;
     return this.system.schema.has("identifier") ? ryuutama.utils.createDefaultIdentifier(this.name) : null;
   }
+
+  /* -------------------------------------------------- */
+
+  /** @override */
+  static getDefaultArtwork(itemData) {
+    let img = this.DEFAULT_ICON;
+
+    switch (itemData.type) {
+      case "accessory":
+      case "armor":
+      case "cape":
+      case "hat":
+      case "shield":
+      case "shoes":
+      case "staff":
+      case "weapon":
+        img = "systems/ryuutama/assets/official/icons/items/equipment.svg";
+        break;
+
+      case "animal":
+        img = "systems/ryuutama/assets/official/icons/items/animal.svg";
+        break;
+
+      case "class":
+        break;
+
+      case "container":
+        img = "systems/ryuutama/assets/official/icons/items/container.svg";
+        break;
+
+      case "herb":
+      case "skill":
+      case "spell":
+        break;
+    }
+
+    return { img };
+  }
 }
