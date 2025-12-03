48bbbd7617bcd07a27b5f9a657766719f3cfd27e
Zhell <kennethzhell@gmail.com>
2025-12-03 13:54:14 +0100

    [#38, #147] Implement usage of 'effect' actions, allowing for granting both effects and statuses.

--- code/data/actions/effect.mjs
+++ code/data/actions/effect.mjs
@@ -4,7 +4,7 @@ import Action from "./action.mjs";
  * @import RyuutamaActiveEffect from "../../documents/active-effect.mjs";
  */
 
-const { NumberField, SchemaField, SetField, StringField, TypedObjectField } = foundry.data.fields;
+const { BooleanField, NumberField, SchemaField, SetField, StringField, TypedObjectField } = foundry.data.fields;
 
 export default class EffectAction extends Action {
   static {
@@ -19,7 +19,8 @@ export default class EffectAction extends Action {
       effects: new SchemaField({
         ids: new SetField(new StringField()),
         statuses: new TypedObjectField(new SchemaField({
-          strength: new NumberField({ integer: true, initial: 6, min: 1, max: 30, nullable: false }),
+          enabled: new BooleanField(),
+          strength: new NumberField({ integer: true, initial: 4, min: 2, max: 20, nullable: false }),
         }), { validateKey: key => key in ryuutama.config.statusEffects }),
       }),
     });
@@ -48,13 +49,18 @@ export default class EffectAction extends Action {
   /** @override */
   async use() {
     const effects = this.applicableEffects;
-    if (!effects.length) {
+    if (!effects.length && !Object.values(this.effects.statuses).some(k => k.enabled)) {
       ui.notifications.error("RYUUTAMA.ITEM.SPELL.warnNoApplicableEffects", { localize: true });
       return null;
     }
     return {
       type: "effect",
       effects: effects.map(e => e.uuid),
+      statuses: Object.fromEntries(
+        Object.entries(this.effects.statuses)
+          .filter(s => s[1].enabled)
+          .map(([k, v]) => [k, v.strength]),
+      ),
     };
   }
 
@@ -65,5 +71,15 @@ export default class EffectAction extends Action {
     super.prepareSheetContext(context);
     context.actionTemplate = "item-action-effect";
     context.actionEffectIds = this.document.effects.map(e => ({ value: e.id, label: e.name }));
+    context.actionStatuses = Object.entries(ryuutama.config.statusEffects)
+      .map(([k, v]) => ({
+        status: k,
+        label: v.name,
+        strength: this.effects.statuses[k]?.strength,
+        enabled: this.effects.statuses[k]?.enabled,
+        namePrefix: `system.action.effects.statuses.${k}.`,
+        strengthField: this.schema.getField("effects.statuses.element.strength"),
+        enabledField: this.schema.getField("effects.statuses.element.enabled"),
+      }));
   }
 }
