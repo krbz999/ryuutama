3f44776ba22981f0f0dea198e67954142b58fbb5
Zhell <kennethzhell@gmail.com>
2025-12-04 18:19:15 +0100

    Move action/roll schema fields into an `ActionData` templating model.

--- code/data/item/spell.mjs
+++ code/data/item/spell.mjs
@@ -1,25 +1,16 @@
-import BaseData from "./templates/base.mjs";
+import ActionData from "./templates/action.mjs";
 
-/**
- * @import RyuutamaChatMessage from "../../documents/chat-message.mjs";
- */
+const { NumberField, SchemaField, StringField } = foundry.data.fields;
 
-const { NumberField, SchemaField, StringField, TypedSchemaField } = foundry.data.fields;
-
-export default class SpellData extends BaseData {
+export default class SpellData extends ActionData {
   /** @override */
   static defineSchema() {
     return Object.assign(super.defineSchema(), {
-      action: new TypedSchemaField(ryuutama.data.action.Action.TYPES, { nullable: true, initial: null, required: true }),
       category: new SchemaField({
         value: new StringField({
           required: true, initial: "incantation",
           choices: () => ryuutama.config.spellCategories,
         }),
-      }),
-      roll: new SchemaField({
-        flavor: new StringField({ required: true }),
-        formula: new ryuutama.data.fields.FormulaField(),
       }),
       spell: new SchemaField({
         activation: new SchemaField({
@@ -82,53 +73,5 @@ export default class SpellData extends BaseData {
   /** @inheritdoc */
   getDamageOptions() {
     return super.getDamageOptions().add("magical");
-  }
-
-  /* -------------------------------------------------- */
-
-  /**
-   * Use this spell.
-   * @returns {Promise<RyuutamaChatMessage|null>}
-   */
-  async use() {
-    const action = this.action;
-    let part;
-    if (action) {
-      part = await action.use();
-      if (!part) return null;
-    }
-
-    const rollPart = await this.#generateGenericRollPart();
-    if (!rollPart) return null;
-
-    const item = this.parent;
-    const messageData = await this.parent.actor.system.rollCheck(
-      { type: "magic", magic: { item } },
-      {},
-      { create: false },
-    );
-    if (!messageData) return null;
-
-    if (rollPart.rolls.length) messageData.system.parts.push(rollPart);
-    if (part) messageData.system.parts.push(part);
-    return getDocumentClass("ChatMessage").create(messageData);
-  }
-
-  /* -------------------------------------------------- */
-
-  /**
-   * Generate the message part for the generic roll.
-   * @returns {Promise<object>}
-   */
-  async #generateGenericRollPart() {
-    const part = {
-      type: "roll", rolls: [],
-      flavor: this.roll.flavor,
-    };
-    if (this.roll.formula) {
-      const roll = await new ryuutama.dice.BaseRoll(this.roll.formula, this.parent.getRollData()).evaluate();
-      part.rolls.push(roll);
-    }
-    return part;
   }
 }
--- code/data/item/templates/_module.mjs
+++ code/data/item/templates/_module.mjs
@@ -1,3 +1,4 @@
 export { default as BaseData } from "./base.mjs";
 export { default as GearData } from "./gear.mjs";
 export { default as PhysicalData } from "./physical.mjs";
+export { default as ActionData } from "./action.mjs";
