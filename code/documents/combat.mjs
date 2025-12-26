7e9685b27ec9aadd1af281a8803f38f2a0548d8e
Zhell <50169243+krbz999@users.noreply.github.com>
2025-12-26 16:23:19 +0100

    [#193] Remove 'Shield Dodge' status and remoe custom AE expiration data (#194)

--- code/documents/combat.mjs
+++ code/documents/combat.mjs
@@ -1,24 +1,8 @@
-/**
- * @import RyuutamaActiveEffect from "./active-effect.mjs";
- */
-
 export default class RyuutamaCombat extends foundry.documents.Combat {
   /** @inheritdoc */
   _initializeSource(data = {}, options = {}) {
     if (!data.type || (data.type === "base")) data.type = "standard";
     return super._initializeSource(data, options);
-  }
-
-  /* -------------------------------------------------- */
-
-  /** @inheritdoc */
-  _onDelete(options, userId) {
-    super._onDelete(options, userId);
-
-    if (!this.active || !game.user.isActiveGM) return;
-
-    // Remove effects that expire upon combat end.
-    this.expireEffects("combatEnd");
   }
 
   /* -------------------------------------------------- */
@@ -32,28 +16,5 @@ export default class RyuutamaCombat extends foundry.documents.Combat {
     }
 
     return this;
-  }
-
-  /* -------------------------------------------------- */
-
-  /**
-   * Expire effects of a given expiration type.
-   * @param {string} type   Expiration type.
-   * @returns {Promise<RyuutamaActiveEffect[]>}   A promise that resolves to the deleted effects.
-   */
-  async expireEffects(type) {
-    if (!game.user.isGM) {
-      throw new Error("Only a GM can expire effects through the Combat document.");
-    }
-
-    const actors = new Set(this.combatants.map(c => c.actor).filter(_ => _));
-    const promises = [];
-    for (const actor of actors) {
-      const effects = actor.effects.filter(effect => effect.system.expiration.type === type);
-      const deleted = actor.deleteEmbeddedDocuments("ActiveEffect", effects.map(effect => effect.id));
-      promises.push(deleted);
-    }
-    const deleted = await Promise.all(promises);
-    return deleted.flat(1);
   }
 }
