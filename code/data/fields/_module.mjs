57b48d8dd8645c8938b44064a27453d9eccfdead
Zhell <kennethzhell@gmail.com>
2026-01-04 14:52:58 +0100

    [#186] Add source fields and config menu to all items and actors, and add identifier field to all item types

--- code/data/fields/_module.mjs
+++ code/data/fields/_module.mjs
@@ -3,3 +3,4 @@ export { default as FormulaField } from "./formula-field.mjs";
 export { default as IdentifierField } from "./identifier-field.mjs";
 export { default as LocalDocumentField } from "./local-document-field.mjs";
 export { default as PseudoDocumentCollectionField } from "./pseudo-document-collection-field.mjs";
+export { default as SourceField } from "./source-field.mjs";
--- code/helpers/prelocalization.mjs
+++ code/helpers/prelocalization.mjs
@@ -16,4 +16,49 @@ export default class Prelocalization {
   static prelocalize(record, options) {
     this.toLocalize.push([record, options]);
   }
+
+  /* -------------------------------------------------- */
+
+  /**
+   * Have sources been configured?
+   * @type {boolean}
+   */
+  static #sourcesConfigured = false;
+
+  /* -------------------------------------------------- */
+
+  /**
+   * Perform a one-time setup of sources from system and module flags.
+   */
+  static configureSources() {
+    if (Prelocalization.#sourcesConfigured) return;
+    Prelocalization.#sourcesConfigured = true;
+
+    const config = ryuutama.config.sources;
+
+    const configureSource = pkg => {
+      const sources = pkg.flags?.[ryuutama.id]?.sources ?? [];
+      for (let { short, long } of sources) {
+        long = game.i18n.localize(long || "");
+        short = short ? game.i18n.localize(short) : long;
+        if (!short || !long) return;
+        if ((short in config) && (config[short] !== long)) {
+          console.warn(`Duplicate source attempted registered: '${short}' / '${long}'`);
+          return;
+        }
+        config[short] = long;
+      }
+    };
+
+    // System
+    configureSource(game.system);
+
+    // Modules
+    for (const module of game.modules) {
+      if (!module.active) continue;
+      configureSource(module);
+    }
+
+    Object.freeze(config);
+  }
 }
