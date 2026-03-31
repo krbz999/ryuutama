8ad38cdbf1c9a174af06acf58aec29fad0e1087a
Zhell <kennethzhell@gmail.com>
2026-04-01 00:20:44 +0200

    Update dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,28 @@
 import postcss from "rollup-plugin-postcss";
 import resolve from "@rollup/plugin-node-resolve";
 import postcssImport from "postcss-import";
-import postcssUrl from "postcss-url";
+import postcssValueParser from "postcss-value-parser";
+
+function adjustCSSUrls() {
+  return {
+    postcssPlugin: "rewrite-system-urls",
+    Declaration(decl) {
+      const parsed = postcssValueParser(decl.value);
+
+      parsed.walk(node => {
+        if ((node.type === "function") && (node.value === "url")) {
+          const urlNode = node.nodes[0];
+          const url = urlNode?.value;
+          if (!url?.startsWith("/systems/ryuutama/")) return;
+          urlNode.value = url.slice("/systems/ryuutama/".length);
+        }
+      });
+
+      decl.value = parsed.toString();
+    },
+  };
+}
+adjustCSSUrls.postcss = true;
 
 export default {
   input: "./_main.mjs",
@@ -14,18 +35,7 @@ export default {
     postcss({
       plugins: [
         postcssImport(),
-        postcssUrl({
-          url: asset => {
-            if (!asset.url) return asset.url;
-            const yeet = "/systems/ryuutama/";
-            if (asset.url.startsWith(yeet)) {
-              return asset.url.slice(yeet.length);
-            } else {
-              console.warn("URL THAT ISN'T IN SYSTEM REPOSITORY:", asset.url);
-            }
-            return asset.url;
-          },
-        }),
+        adjustCSSUrls(),
       ],
       extract: true,
     }),
